8b06a60bb31439d88ed3e1f7ccd3b7b067444a12
Curtis <clpage.15@gmail.com>
2013-01-30 13:58:28 -0800

    Done and accepted

--- HanoiTowerTroublesAgain/src/Main.java
+++ HanoiTowerTroublesAgain/src/Main.java
@@ -32,16 +32,19 @@ class myStuff implements Runnable {
     public void run() {
 
         Scanner scan = new Scanner(System.in);
+        Scanner lineReader;
         int runs;
         StringBuilder output = new StringBuilder();
 
         //The first line of the input contains a single integer T indicating the number of test cases (1 ≤ T ≤ 50).
-        runs = scan.nextInt();
+        lineReader = new Scanner(scan.nextLine());
+        runs = lineReader.nextInt();
 
         for (int runCount = 0; runCount < runs; runCount++)
         {
             //Each test case contains a single integer N (1 ≤ N ≤ 50) indicating the number of pegs available.
-            int pegs = scan.nextInt();
+            lineReader = new Scanner(scan.nextLine());
+            int pegs = lineReader.nextInt();
 
             int[] hanoiTowers = new int[pegs];
 
@@ -55,13 +58,14 @@ class myStuff implements Runnable {
             //This version consists of N pegs and one ball containing each number from 1, 2, 3, . . . , ∞.
             while (true)
             {
-                System.out.println(hanoiTowers.length);
+                //System.out.println(hanoiTowers.length);
 
                 for (int currentPeg = 0; currentPeg < hanoiTowers.length; currentPeg++)
                 {
-                    if (isPerfectSquare(hanoiTowers[currentPeg] + ball))
+                    wasPlaced = false;
+                    if (hanoiTowers[currentPeg] == 0 || isPerfectSquare(hanoiTowers[currentPeg] + ball))
                     {
-                        //Add ball to peg
+                        //Add ball to empty peg
                         hanoiTowers[currentPeg] = ball;
 
                         //Indicate ball was placed and count
@@ -71,12 +75,16 @@ class myStuff implements Runnable {
                         //Move on to next ball
                         break;
                     }
+
                 }
 
                 //Ball wasn't placed; break out of loop and print
                 if (!wasPlaced) {
-                    output.append("\n");
                     output.append(ballCount);
+
+                    if(runCount < runs)
+                        output.append("\n");
+
                     break;
                 }
                 //Ball was placed; continue loop and grab next ball
@@ -88,7 +96,7 @@ class myStuff implements Runnable {
             }
         }
 
-        System.out.print(output);
+        System.out.print(output.toString());
     }
 
     public final static boolean isPerfectSquare(int n) {
