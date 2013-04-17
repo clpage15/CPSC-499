5172519c1bbdf192000baa05e54556fb3072c639
Alex Reed <areed1@unbc.ca>
2013-04-16 19:45:25 -0700

    Intergration; Alex got partially working

--- Intergration/src/Main.java
+++ Intergration/src/Main.java
@@ -1,6 +1,6 @@
 /**
  * Created with IntelliJ IDEA.
- * User: curtis
+ * User: Curtis
  * Date: 2013-01-22
  * Time: 1:35 PM
  * To change this template use File | Settings | File Templates.
@@ -16,7 +16,6 @@ import java.util.*;
 
 class Main implements Runnable{
 
-
     public static void main(String args[])
     {
         Main myWork = new Main();
@@ -31,20 +30,29 @@ class myStuff implements Runnable{
 
     public void run()
     {
-        Scanner scan = new Scanner(System.in);
+        String nextLine = null;
         StringBuilder output = new StringBuilder();
 
-        while(scan.hasNextDouble())
+        Scanner scan = new Scanner(System.in);
+
+        while(scan.hasNextLine())
         {
-            double squareRadius = scan.nextDouble();
+            nextLine  = scan.nextLine();
+
+            Scanner line = new Scanner(nextLine);
+
+            double squareRadius = line.nextDouble();
+
+            // We will need the area of the square later, let's store this value for now
             double squareArea = squareRadius * squareRadius;
+
+            // See this link to understand circular cutouts...http://skepticsplay.blogspot.ca/2011/02/solutions-to-circular-cutouts.html
+            // Using the diagram and rational in above link, we can calculate the center.
             double center = (Math.PI / 3 + 1 - Math.sqrt(3)) * squareArea;
             double leaves = (Math.PI / 3 - 4 + 2 * Math.sqrt(3)) * squareArea;
             double square = (4 - (2 * Math.PI) / 3 - Math.sqrt(3)) * squareArea;
 
-            output.append(String.format("%.3f %.3f %.3f\n", center,leaves,square));
+            System.out.print(String.format("%.3f %.3f %.3f\n", center,leaves,square));
         }
-
-        System.out.println(output.toString());
     }
 }
